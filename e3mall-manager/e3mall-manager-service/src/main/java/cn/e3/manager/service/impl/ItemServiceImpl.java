b3c48ef0531f673776729b34147240c8673968f4
changjiangshan <13137437087@163.com>
2017-12-07 19:49:17 +0800

    e3mall-manager

--- e3mall-manager/e3mall-manager-service/src/main/java/cn/e3/manager/service/impl/ItemServiceImpl.java
+++ e3mall-manager/e3mall-manager-service/src/main/java/cn/e3/manager/service/impl/ItemServiceImpl.java
@@ -1,11 +1,18 @@
 package cn.e3.manager.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 
 import cn.e3.manager.service.ItemService;
 import cn.e3.mapper.TbItemMapper;
 import cn.e3.pojo.TbItem;
+import cn.e3.pojo.TbItemExample;
+import cn.e3.utils.DatagridPagebean;
 @Service
 public class ItemServiceImpl implements ItemService {
 
@@ -16,6 +23,24 @@ public class ItemServiceImpl implements ItemService {
 	public TbItem findItemById(Long id) {
 		TbItem ibitem = ibItemMapper.selectByPrimaryKey(id);
 		return ibitem;
+	}
+
+	@Override
+	public DatagridPagebean findItemBypage(Integer page, Integer rows) {
+		TbItemExample example = new TbItemExample();
+	 
+	     PageHelper.startPage(page,rows);
+		List<TbItem> list = ibItemMapper.selectByExample(example);
+		//创创建一个获取分页信息
+		PageInfo<TbItem> pageInfo = new PageInfo<TbItem>(list);
+		//获取总记录数
+		long total = pageInfo.getTotal();
+		//创建一个返回值对象
+		DatagridPagebean pageBean = new DatagridPagebean();
+		//设置查询分页记录
+		pageBean.setRows(list);
+		pageBean.setTotal(total);
+		return pageBean;
 	}
 
 }
